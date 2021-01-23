e0d1f8395bfb9d244622c1fbf1bd3c83a08b96d9
dana <2280518818@qq.com>
2021-01-23 20:53:54 +0800

    feat(init): init

---
+++ mybatis-demo-01/src/test/java/com/test/AppTest.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import static org.junit.Assert.assertTrue;
+
+import com.test.mapper.UserMapper;
+import com.test.pojo.User;
+import com.test.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> all = mapper.getAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+    }
+}
